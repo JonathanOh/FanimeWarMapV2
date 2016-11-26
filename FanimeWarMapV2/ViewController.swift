936ff6cd366c973a8f067c4497b3458f066b17c3
Jonathan Oh <jonoh@yelp.com>
2016-11-25 23:22:27 -0800

    Initial Commit

---
+++ FanimeWarMapV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FanimeWarMapV2
+//
+//  Created by Jonathan Oh on 11/25/16.
+//  Copyright © 2016 Jonathan Oh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
